0907c07275878ad33c339d608d6187236f19e5ed
patrickdobler <paddy@hotmail.ch>
2022-06-07 23:37:03 +0200

    adding section to separate base system pkgs

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -89,6 +89,26 @@ if [[ ! $DESKTOP_ENV == server ]]; then
     sudo pacman -S --noconfirm --needed ${line}
   done
 fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Extra Software  
+-------------------------------------------------------------------------
+"
+# sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+# stop the script and move on, not installing any more packages below that line
+if [[ ! $DESKTOP_ENV == server ]]; then
+  sed -n '/'$INSTALL_TYPE'/q;p' $HOME/Arch123/pkg-files/pacman-extra-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    sudo pacman -S --noconfirm --needed ${line}
+  done
+fi
+
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Microcode
