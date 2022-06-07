615e040c621c2157ceb2e81fc20a0c6c87c63c7e
patrickdobler <paddy@hotmail.ch>
2022-06-07 18:01:10 +0200

    add swissgerman keyboard

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -209,7 +209,7 @@ keymap () {
 echo -ne "
 Please select key board layout from this list"
 # These are default key maps as presented in official arch repo archinstall
-options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
+options=(us by ca cf ch cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
 
 select_option $? 4 "${options[@]}"
 keymap=${options[$?]}
